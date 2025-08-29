05aaf5a1726a5f1c806353b33270f093648db3ce
Promit Bhar <bharpromit@gmail.com>
2025-08-29 16:55:16 +0530

    Updated all the files

--- android/app/src/main/supabase.js
+++ android/app/src/main/supabase.js
@@ -1,6 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseAnon = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
-
-export const supabase = createClient(supabaseUrl, supabaseAnon);
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://oafmvcbetnccbrzdbjiv.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
